6cfd02eaa8303569a6ac6f92a5ef7cb6f0a9429c
Anna <anna.dvoskina@yandex.ru>
2021-10-21 11:01:43 +0300

    Revert "added linters"
    This reverts commit f3c00b23a5224a92cc7373f88565b33f1454ec76.

--- service/__main__.py
+++ service/__main__.py
@@ -1,8 +1,11 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import httpx
+from flask import Flask, jsonify, request, abort
 import service.config
 
+app = Flask(__name__)
+client = app.test_client()
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -16,93 +19,32 @@ PROXY = {
     }
 }
 
-
 def greet_user(update, context):
-    update.message.reply_text(
-        'Привет! Я помогу тебе найти водопад!\n \
-        Если знаешь название водопада, отправь мне его в формате:\n \
-        /name "название"\n \
-        Или же давай попробуем найти водопад по описанию:\n \
-        /description "то, что ты знаешь о водопаде"'
-        )
+    text = 'Вызван /start'
+    update.message.reply_text('Привет! Я помогу тебе найти водопад!\nОтправь мне водопад в формате:\n/waterfall твой водопад')
 
+# def default_response(update, context):
+#     user_text = update.message.text
+#     text = f'Привет! Я помогу тебе найти водопад!\nОтправь мне водопад в формате:\n/waterfall твой водопад'
+#     update.message.reply_text(text)
 
-def get_waterfalls_by_name(update, context):
+def get_waterfalls(update, context):
     text = update.message.text
-    text = text.replace('/name ', '')
+    text = text.replace('/waterfall','')
     logging.info(text)
-    title = text
-    detail = None
-    waterfall = httpx.get(f'http://127.0.0.1:5000/api/v1/waterfalls?detail={detail}&title={title}')
+    waterfall = httpx.get('http://127.0.0.1:5000/api/v1/waterfalls/')
     waterfalls = waterfall.json()
-    if waterfalls:
-        for waterfall in waterfalls:
-            update.message.reply_text(
-                str(
-                    f'Название: {waterfall.get("title", "")}\n \
-Страна: {waterfall.get("country", "")}\n \
-Регион: {waterfall.get("region", "")}\n \
-Субъект РФ: {waterfall.get("RF_subject", "")}\n \
-Река: {waterfall.get("river", "")}\n \
-Высота: {waterfall.get("height", "")}\n \
-Ширина: {waterfall.get("width", "")}\n \
-{waterfall["url"]}'
-                ))
-
-    else:
-        update.message.reply_text(str('Такого водопада я не знаю :('))
-
-
-def get_waterfalls_by_desc(update, context):
-    text = update.message.text
-    text = text.replace('/description ', '')
-    logging.info(text)
-    title = None
-    detail = text
-    waterfall = httpx.get(f'http://127.0.0.1:5000/api/v1/waterfalls?detail={detail}&title={title}')
-    waterfalls = waterfall.json()
-    if waterfalls:
-        for waterfall in waterfalls:
-            update.message.reply_text(
-                str(
-                    f'Название: {waterfall.get("title", "")}\n \
-Страна: {waterfall.get("country", "")}\n \
-Регион: {waterfall.get("region", "")}\n \
-Субъект РФ: {waterfall.get("RF_subject", "")}\n \
-Река: {waterfall.get("river", "")}\n \
-Высота: {waterfall.get("height", "")}\n \
-Ширина: {waterfall.get("width", "")}\n \
-{waterfall["url"]}'
-                ))
-    else:
-        update.message.reply_text(str('Попробуй немного изменить описание :)'))
-
-
-def default_response(update, context):
-    update.message.reply_text(
-        'Привет! Я помогу тебе найти водопад!\n \
-        Если знаешь название водопада, отправь мне его в формате:\n \
-        /name "название"\n \
-        Или же давай попробуем найти водопад по описанию:\n \
-        /description "то, что ты знаешь о водопаде"'
-        )
+    update.message.reply_text(str(waterfalls))
 
 
 def main():
-    mybot = Updater(
-                    service.config.TOKEN,
-                    request_kwargs=PROXY,
-                    use_context=True
-                    )
-    dp = mybot.dispatcher
+    mybot = Updater(service.config.TOKEN, request_kwargs=PROXY, use_context=True)
+    dp = mybot.dispatcher    
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("name", get_waterfalls_by_name))
-    dp.add_handler(CommandHandler("description", get_waterfalls_by_desc))
-    dp.add_handler(MessageHandler(Filters.text, default_response))
+    dp.add_handler(CommandHandler("waterfall", get_waterfalls))
+    # dp.add_handler(CommandHandler(Filters.text, default_response))
 
     mybot.start_polling()
     mybot.idle()
-
-
 if __name__ == "__main__":
     main()
